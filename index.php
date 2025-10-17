28e57894367ebf29580a0dcb2d317d4b8f006ab3
Khalid <118032316+RAZOR1224@users.noreply.github.com>
2025-10-17 14:16:10 +0700

    Update index.php
    a

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Web1 - Aplikasi 1</h1>";
+echo "<h1>Web toram - Aplikasi 1</h1>";
 echo "<p>Server IP: " . $_SERVER['SERVER_ADDR'] . " | Port: 2000</p>";
 echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
 ?>
